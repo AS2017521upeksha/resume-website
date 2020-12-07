fdc941965306f31f5874946f1b9c96953a66bd75
upeksha <upekshasamarasingher@gmail.com>
2020-12-07 23:27:45 +0530

    works page done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutMeComponent } from './components/about-me/about-me.component';
 import { ResumeComponent } from './components/resume/resume.component';
+import { WorksComponent } from './components/works/works.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 
@@ -23,6 +24,10 @@ const routes: Routes = [
       {
         path : 'resume',
         component : ResumeComponent
+      },
+      {
+        path : 'works',
+        component : WorksComponent
       }
     ]
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { AboutMeComponent } from './components/about-me/about-me.component';
 import { ResumeComponent } from './components/resume/resume.component';
 import { WorksComponent } from './components/works/works.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import { WorksComponent } from './components/works/works.component';
     DashboardComponent,
     AboutMeComponent,
     ResumeComponent,
-    WorksComponent],
+    WorksComponent,
+    ContactComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
