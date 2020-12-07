700336a1d0b134599e3286e4f6bcc654942ccaba
upeksha <upekshasamarasingher@gmail.com>
2020-12-07 23:45:12 +0530

    contact pahe created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ContactComponent } from './components/contact/contact.component';
 import { ResumeComponent } from './components/resume/resume.component';
 import { WorksComponent } from './components/works/works.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
@@ -28,6 +29,10 @@ const routes: Routes = [
       {
         path : 'works',
         component : WorksComponent
+      },
+      {
+        path : 'contact',
+        component : ContactComponent
       }
     ]
   }
